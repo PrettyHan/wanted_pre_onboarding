4b57770299d7e07f0241e74c619dccb63efe5fea
PrettyHan <imbel9830@gmail.com>
2022-08-18 19:37:03 +0900

    add: wanted api문서 작성(swagger)

---
+++ src/util/setupSwagger.ts
@@ -0,0 +1,18 @@
+import { INestApplication } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+/**
+ * Swagger 세팅
+ *
+ * @param {INestApplication} app
+ */
+export function setupSwagger(app: INestApplication): void {
+  const options = new DocumentBuilder()
+    .setTitle('원티드 프리 온보딩 API')
+    .setDescription('원티드 온보딩 과제에 대한 API 명세서 입니다.')
+    .setVersion('1.0.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api-docs', app, document);
+}
--- src/wanted/wanted.controller.ts
+++ src/wanted/wanted.controller.ts
@@ -7,33 +7,89 @@ import {
   Patch,
   Delete,
 } from '@nestjs/common';
+import {
+  ApiBody,
+  ApiCreatedResponse,
+  ApiNoContentResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiParam,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { CreateWantedDto, UpdateWantedDto } from './dto';
+import { wantedEntity } from './entity/wanted.entity';
 import { WantedService } from './wanted.service';
 
+@ApiTags('채용공고 API')
 @Controller('wanted')
 export class WantedController {
   constructor(private readonly wantedService: WantedService) {}
 
+  @ApiOperation({
+    summary: '채용공고 생성 API',
+    description: '채용공고 생성',
+  })
+  @ApiBody({ type: CreateWantedDto })
+  @ApiCreatedResponse({ description: '채용공고 생성', type: wantedEntity })
   @Post()
   async create(@Body() new_wanted: CreateWantedDto) {
     return this.wantedService.create(new_wanted);
   }
 
+  @ApiOperation({
+    summary: '모든 채용공고 리스트 불러오기 API',
+    description: '채용공고 리스트 불러옴',
+  })
+  @ApiOkResponse({
+    status: 200,
+    isArray: true,
+    type: wantedEntity,
+  })
   @Get('list')
   async findAll() {
     return await this.wantedService.findAll();
   }
+
+  @ApiOperation({
+    summary: '채용공고 찾기 API',
+    description: '채용공고 id 받고 해당 채용공고 불러옴',
+  })
+  @ApiOkResponse({ type: wantedEntity })
+  @ApiParam({
+    name: 'id',
+    required: true,
+  })
   @Get(':id')
   async findOne(@Param('id') id: number) {
     console.log(id);
     return await this.wantedService.findOne(id);
   }
 
+  @ApiOperation({
+    summary: '채용공고 수정 API',
+    description: '채용공고 id 받고 해당 채용공고 수정함',
+  })
+  @ApiCreatedResponse({ type: wantedEntity })
+  @ApiParam({
+    name: 'id',
+    required: true,
+  })
   @Patch(':id')
   async patch(@Param('id') id: number, @Body() updateData: UpdateWantedDto) {
     return await this.wantedService.update(id, updateData);
   }
 
+  @ApiOperation({
+    summary: '채용공고 삭제 API',
+    description: '채용공고 id 받고 해당 채용공고 삭제함',
+  })
+  @ApiNoContentResponse({ description: '삭제' })
+  @ApiParam({
+    name: 'id',
+    required: true,
+  })
   @Delete(':id')
   async remove(@Param('id') id: number) {
     return await this.wantedService.remove(id);
